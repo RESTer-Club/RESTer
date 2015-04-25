687532b9bb97ceb5adc62875f15b0a754ed1d779
theqaangel <angel.tsvetkov@hotmail.com>
2015-04-25 09:38:56 +0300

    finish request body

--- app/common/directives/request-body/request-body.js
+++ app/common/directives/request-body/request-body.js
@@ -6,7 +6,6 @@ app.directive('requestBody', function () {
         replace: true,
         templateUrl: 'common/directives/request-body/request-body-template.html',
         link: function (scope) {
-
             var _requestBody;
 
             initRequestBody();
@@ -24,61 +23,26 @@ app.directive('requestBody', function () {
             }
 
             scope.resetBodyEditor = function () {
-                _requestBody.getDoc().setValue('');
-            }
+                if (_requestBody) {
+                    _requestBody.getDoc().setValue('');
+                }
+            };
 
             scope.refreshBodyEditor = function () {
                 //Need some time to expand the container prior to refresh the editor
                 setTimeout(function () {
                     _requestBody.refresh();
+                    _requestBody.focus();
                 }, 100);
-            }
+            };
 
-            _requestBody.on("change", function (cm, change) {
+            _requestBody.on("change", function (cm, change) { // jshint ignore:line
+                if (typeof (_requestBody) === 'undefined') {
+                    return;
+                }
+
                 scope.request.body = _requestBody.getValue();
             });
-
-            //            scope.isHeaderAreaExpanded = false;
-            //
-            //            scope.expandHeaders = function () {
-            //                    var headersCount = scope.request.headers.length;
-            //                    if (headersCount === 0 && !scope.isHeaderAreaExpanded) {
-            //                        //Add empty fields if there are no any
-            //                        scope.request.headers.push({
-            //                            name: "",
-            //                            value: ""
-            //                        });
-            //                    } else {
-            //                        //Remove header if there is only one and empty
-            //                        if (scope.request.headers.length === 1) {
-            //                            var header = scope.request.headers[0];
-            //                            if (header.name.length === 0 && header.value.length === 0 && scope.isHeaderAreaExpanded) {
-            //                                scope.request.headers.splice(0, 1);
-            //                            }
-            //                        }
-            //                    }
-            //
-            //                    scope.isHeaderAreaExpanded = !scope.isHeaderAreaExpanded;
-            //                },
-            //
-            //                scope.addNewHeader = function () {
-            //                    scope.request.headers.push({
-            //                        name: "",
-            //                        value: ""
-            //                    });
-            //                },
-            //
-            //                scope.deleteHeader = function (position) {
-            //                    if (scope.request.headers.length > 0) {
-            //                        if (position > 0 || scope.request.headers.length > 1) {
-            //                            scope.request.headers.splice(position, 1);
-            //                        } else if (position === 0) {
-            //                            var header = scope.request.headers[0];
-            //                            header.name = "";
-            //                            header.value = "";
-            //                        }
-            //                    }
-            //                }; // jshint ignore:line
         }
     };
 });
